de295915bf94fc6fbf7922b03aaae02e61218f1d
FranDev <91391986+codigo04@users.noreply.github.com>
2025-07-13 13:25:20 -0500

    Update App.java

--- prueba/App.java
+++ prueba/App.java
@@ -10,24 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        ClientConfigError config = ClientConfigError
-                .builder()
-                .apiKey("AIzaSyAddflSIMxgJVe-syg-rket-0XbB7kYckA")
-                .build();
-        ErrorAssistantIA.withConfig(config);
-
-        try {
-
-            int numero = 10 / 0; // fuerza error
-
-//            String texto = null;
-//            System.out.println(texto.length()); // fuerza error
-        } catch (Exception e) {
-            System.out.println(ErrorAssistantIA.analizaError(e).getDescripcion());
-
-
-        }
     }
 }
 
